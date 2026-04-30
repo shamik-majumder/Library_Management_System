12e134a1ab5fc35490df47b2a0a94cd736e2ebe2
SHAMIK MAJUMDER <shamikmajumder2001bln@gmail.com>
2026-05-01 00:14:37 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy;Online Library Management System For My IGNOU Project (BCSP064) |<a href="https://itsourcecode.com/" target="_blank"  > Designed by : Shamik Majumder</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
